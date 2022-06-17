aa7e330b56485ff77ee0cb7c86d68382a363ed84
雷霆嘎巴 <1453533432@qq.com>
2022-06-17 14:01:30 +0800

    动态字符题

---
+++ test_6_17/dimo1.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+
+
+
+
+//¶¯Ì¬×Ö·ûÌâ
+//#include <stdio.h>
+//#include <string.h>
+//#include <windows.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	char arr1[] = "This is a C program !";
+//	char arr2[] = "#####################";
+//	int left = 0;
+//	int right =sizeof(arr1)/sizeof(arr1[0])-2;
+//	while (left <= right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		Sleep(1000);//Í£Áô1Ãë
+//		system("cls");//Çå¿ÕÆÁÄ»
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", arr2);
+//	return 0;
+//}
