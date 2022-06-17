1764e5cc501831bcddf85d5c3e684ca2366eb4b2
雷霆嘎巴 <1453533432@qq.com>
2022-06-17 15:28:59 +0800

    求闰年、公约数，排序--三题

--- test_6_17/dimo1.c
+++ test_6_17/dimo1.c
@@ -1,11 +1,90 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include <stdio.h>
 
 
 
 
-
-
+////求闰年两种写法
+//int main()
+//{
+//	int a = 0;
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		/*if (year % 4 == 0 && year % 100 != 0)
+//		{
+//			printf("%d ", year);
+//			a++;
+//		}
+//		if (year % 400 == 0)
+//		{
+//			printf("%d ", year);
+//			a++;
+//		}*/
+//		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+//		{
+//			printf("%d ", year);
+//			a++;
+//		}
+//	}
+//	printf("\n共有：%d年\n", a);
+//	return 0;
+//}
+//
+////求两数最大公约数
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d", &a, &b);
+//	while (a % b)       //取模是否为0，直接使用
+//	{
+//		c = a % b;
+//		a = b;
+//		b = c;
+//	}
+//	printf("最大公约数是：%d\n", b);
+//	return 0;
+//}
+//
+//三数排序题
+//输入需用空格隔开
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//
+//	if (a < b)
+//	{
+//		int d = a;
+//		a = b;
+//		b = d;
+//     }
+//	if (a < c)
+//	{
+//		int d = a;
+//		a = c;
+//		c = d;
+//	}
+//	if (b < c)
+//	{
+//		int d = b;
+//		b = c;
+//		c = d;
+//	}
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//}
+//
+//
+//
+//
 //动态字符题
 //#include <stdio.h>
 //#include <string.h>
@@ -18,6 +97,7 @@
 //	char arr2[] = "#####################";
 //	int left = 0;
 //	int right =sizeof(arr1)/sizeof(arr1[0])-2;
+// //int right = strlen(arr1) - 1;
 //	while (left <= right)
 //	{
 //		arr2[left] = arr1[left];
