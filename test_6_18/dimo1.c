8f2630b11d286246f1ca1633cb32747e0a65e99d
雷霆嘎巴 <1453533432@qq.com>
2022-06-19 14:25:28 +0800

    乘法口诀表

--- test_6_18/dimo1.c
+++ test_6_18/dimo1.c
@@ -4,28 +4,47 @@
 #include <math.h>
 
 
-//100-200之间素数题
+
+
+//打印乘法口诀表
 int main()
 {
 	int a = 0;
-	int count = 0;
-	for (a = 100; a <= 200; a++)
+	for (a = 1; a <= 9; a++)
 	{
 		int b = 0;
-		//sqrt-开平方的数学库函数
-		for (b = 2; b <= sqrt(a); b++)
+		for (b = 1; b <= a; b++)
 		{
-			if (a % b == 0)
-			{
-				break;
-			}
+			printf("%d*%d=%-2d ", a, b, a * b);
 		}
-		if (b > sqrt(a))
-		{
-			count++;
-			printf("%d ", a);
-		}
+		//%-2d向左对齐   %2d向右对齐
+		printf("\n");
 	}
-	printf("\ncount=%d\n", count);
 	return 0;
 }
+
+////100-200之间素数题
+//int main()
+//{
+//	int a = 0;
+//	int count = 0;
+//	for (a = 100; a <= 200; a++)
+//	{
+//		int b = 0;
+//		//sqrt-开平方的数学库函数
+//		for (b = 2; b <= sqrt(a); b++)
+//		{
+//			if (a % b == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (b > sqrt(a))
+//		{
+//			count++;
+//			printf("%d ", a);
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
